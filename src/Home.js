a9379b13cff8d16b0a05545a43c0c25f367a9675
alfism <alfialfarisi@gmail.com>
2021-05-21 15:55:17 +0700

    Test Firebase CI

--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,7 @@ function Home() {
           <div className="__col2">
             <Product
               id="12321341"
-              title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+              title="The Lean Okays Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
               price={11.96}
               rating={5}
               image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
