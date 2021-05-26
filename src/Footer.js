311f3581a03615a868ef8c76af74873753b18433
alfism <alfialfarisi@gmail.com>
2021-05-26 19:21:05 +0700

    Add snackbar function on add product and modify footer

--- src/Footer.js
+++ src/Footer.js
@@ -67,10 +67,10 @@ function Footer() {
           <div className="__col4">
             <h4>Make Money with Us</h4>
             <ul>
-              <li class="nav_first">
+              <li className="nav_first">
                 <a
                   href="https://services.amazon.com/sell.html?ld=AZFSSOA&amp;ref_=footer_soa"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Sell products on Amazon
                 </a>
@@ -78,25 +78,25 @@ function Footer() {
               <li>
                 <a
                   href="https://services.amazon.com/amazon-business.html?ld=usb2bunifooter&amp;ref_=footer_b2b"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Sell on Amazon Business
                 </a>
               </li>
               <li>
-                <a href="https://developer.amazon.com" class="nav_a">
+                <a href="https://developer.amazon.com" className="nav_a">
                   Sell apps on Amazon
                 </a>
               </li>
               <li>
-                <a href="https://affiliate-program.amazon.com/" class="nav_a">
+                <a href="https://affiliate-program.amazon.com/" className="nav_a">
                   Become an Affiliate
                 </a>
               </li>
               <li>
                 <a
                   href="https://advertising.amazon.com/?ref=ext_amzn_ftr"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Advertise Your Products
                 </a>
@@ -104,7 +104,7 @@ function Footer() {
               <li>
                 <a
                   href="/gp/seller-account/mm-summary-page.html?ld=AZFooterSelfPublish&amp;topic=200260520&amp;ref_=footer_publishing"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Self-Publish with Us
                 </a>
@@ -112,16 +112,16 @@ function Footer() {
               <li>
                 <a
                   href="http://go.thehub-amazon.com/amazon-hub-locker"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Host an Amazon Hub
                 </a>
               </li>
-              <li class="nav_last nav_a_carat">
-                <span class="nav_a_carat">›</span>
+              <li className="nav_last nav_a_carat">
+                <span className="nav_a_carat">›</span>
                 <a
                   href="/b/?node=18190131011&amp;ld=AZUSSOA-seemore&amp;ref_=footer_seemore"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   See More Make Money with Us
                 </a>
@@ -132,10 +132,10 @@ function Footer() {
           <div className="__col4">
             <h4>Amazon Payment Products</h4>
             <ul>
-              <li class="nav_first">
+              <li className="nav_first">
                 <a
                   href="/dp/B07984JN3L?plattr=ACOMFO&amp;ie=UTF-8"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Amazon Business Card
                 </a>
@@ -143,7 +143,7 @@ function Footer() {
               <li>
                 <a
                   href="/gp/browse.html?node=16218619011&amp;ref_=footer_swp"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Shop with Points
                 </a>
@@ -151,15 +151,15 @@ function Footer() {
               <li>
                 <a
                   href="/gp/browse.html?node=10232440011&amp;ref_=footer_reload_us"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Reload Your Balance
                 </a>
               </li>
-              <li class="nav_last ">
+              <li className="nav_last ">
                 <a
                   href="/gp/browse.html?node=388305011&amp;ref_=footer_tfx"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Amazon Currency Converter
                 </a>
@@ -170,10 +170,10 @@ function Footer() {
           <div className="__col4">
             <h4>Let Us Help You</h4>
             <ul>
-              <li class="nav_first">
+              <li className="nav_first">
                 <a
                   href="/gp/help/customer/display.html?nodeId=GDFU3JS5AL6SYHRD&amp;ref_=footer_covid"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Amazon and COVID-19
                 </a>
@@ -181,7 +181,7 @@ function Footer() {
               <li>
                 <a
                   href="https://www.amazon.com/gp/css/homepage.html?ref_=footer_ya"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Your Account
                 </a>
@@ -189,7 +189,7 @@ function Footer() {
               <li>
                 <a
                   href="https://www.amazon.com/gp/css/order-history?ref_=footer_yo"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Your Orders
                 </a>
@@ -197,7 +197,7 @@ function Footer() {
               <li>
                 <a
                   href="/gp/help/customer/display.html?nodeId=468520&amp;ref_=footer_shiprates"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Shipping Rates &amp; Policies
                 </a>
@@ -205,7 +205,7 @@ function Footer() {
               <li>
                 <a
                   href="/gp/css/returns/homepage.html?ref_=footer_hy_f_4"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Returns &amp; Replacements
                 </a>
@@ -213,7 +213,7 @@ function Footer() {
               <li>
                 <a
                   href="/gp/digital/fiona/manage?ref_=footer_myk"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Manage Your Content and Devices
                 </a>
@@ -221,15 +221,15 @@ function Footer() {
               <li>
                 <a
                   href="/gp/BIT/ref=footer_bit_v2_us_A0029?bitCampaignCode=A0029"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Amazon Assistant
                 </a>
               </li>
-              <li class="nav_last ">
+              <li className="nav_last ">
                 <a
                   href="/gp/help/customer/display.html?nodeId=508510&amp;ref_=footer_gw_m_b_he"
-                  class="nav_a"
+                  className="nav_a"
                 >
                   Help
                 </a>
@@ -239,7 +239,19 @@ function Footer() {
         </div>
       </div>
 
-      <div className="footer__bottom"></div>
+      <div className="footer__bottom">
+        <img
+          className="footer__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+
+        <div className="footer__btnmenu">
+          <button>English</button>
+          <button>USD - U.S Dollar</button>
+          <button>United States</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./Product";
 import "./Test.css";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+// import IconButton from "@material-ui/core/IconButton";
+// import CloseIcon from "@material-ui/icons/Close";
+import { Alert } from "@material-ui/lab";
 
 function Test() {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <div className="row">
@@ -47,7 +62,49 @@ function Test() {
         </div>
       </div>
 
-      <div className="test">Hehe</div>
+      <Button onClick={handleClick}>Open me! {open}</Button>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        style={{
+          zIndex: 9999,
+          marginTop: "70px",
+        }}
+      >
+        <Alert onClose={handleClose} severity="success">
+          This is a success messadge!
+        </Alert>
+      </Snackbar>
+      {/* <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={open}
+        onClose={handleClose}
+        message="Note archived!"
+        style={{zIndex: 99999999}}
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              UNDO
+            </Button>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      /> */}
     </div>
   );
 }
